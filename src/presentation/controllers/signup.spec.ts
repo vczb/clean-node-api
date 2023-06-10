23bcaac620497ca608eb0f7cbcd4c17374bf8a7a
Vinicius CZB <viniczb@gmail.com>
2023-06-10 20:46:45 -0300

    test: ensure SignUpController returns 400 if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,16 @@
-describe("Jest setup", () => {
-  test("testing jest", () => {
-    expect(1).toBe(1);
+import { SignUpController } from "./signup";
+
+describe("SignUp Controller", () => {
+  test("Should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "password",
+        password_confirmation: "password_confirmation",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
   });
 });
