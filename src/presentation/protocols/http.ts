066dce0ff413bdf606efee14129e92f8b2922833
Vinicius CZB <viniczb@gmail.com>
2023-06-11 12:18:39 -0300

    refactor: ignore type any on http protocol

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,8 +1,10 @@
 export interface HttpResponse {
   statusCode: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   body: any;
 }
 
 export interface HttpRequest {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   body?: any;
 }
