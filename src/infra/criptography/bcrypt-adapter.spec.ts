9937e90af64ddb5d6822f6f70c4000c99e42cb31
Vinicius CZB <viniczb@gmail.com>
2023-06-15 19:54:00 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -14,6 +14,9 @@ const makeSut = (): BcryptAdapter => {
 };
 
 describe("Bcrypt Adapter", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
   test("Should call bcrypt with correct values", async () => {
     const sut = makeSut();
     const hashSpy = jest.spyOn(bcrypt, "hash");
@@ -25,5 +28,19 @@ describe("Bcrypt Adapter", () => {
     const sut = makeSut();
     const hash = await sut.encrypt("any_value");
     expect(hash).toBe("hash");
+  });
+
+  test("Should throw if bcrypt throws", async () => {
+    const { encrypt } = makeSut();
+    jest.spyOn(bcrypt, "hash").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+    const promise = encrypt(accountData.password);
+    await expect(promise).rejects.toThrow();
   });
 });
