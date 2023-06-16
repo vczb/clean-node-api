74a3af3cd85631caddb566eeb9c45781b1106b0b
Vinicius CZB <viniczb@gmail.com>
2023-06-15 21:19:33 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/accout-repository/account.ts
@@ -0,0 +1,22 @@
+import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
+import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usecases/add-account";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+
+    const result = await accountCollection.insertOne(accountData);
+    const data = await accountCollection.findOne({
+      _id: result.insertedId,
+    });
+
+    const { _id, ...obj } = data;
+    return {
+      // @ts-expect-error
+      id: _id,
+      ...obj,
+    };
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { Collection, MongoClient } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -7,5 +7,9 @@ export const MongoHelper = {
   },
   async disconnect(): Promise<void> {
     this.client.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
   },
 };
