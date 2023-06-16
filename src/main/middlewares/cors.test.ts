410174b5ece26812545f8dca9eced58f7926cd2b
Vinicius CZB <viniczb@gmail.com>
2023-06-16 20:31:35 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import app from "../config/app";
+
+describe("CORS Middleware", () => {
+  test("Should enable cors", async () => {
+    const endpoint = "/test_cors";
+    app.post(endpoint, (req, res) => {
+      res.send();
+    });
+    await request(app).get(endpoint).expect("access-control-allow-origin", "*");
+  });
+});
