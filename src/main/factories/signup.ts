048a970e9b88d6dbb71b23cc06edfb8dc95c06b6
Vinicius CZB <viniczb@gmail.com>
2023-06-17 20:23:24 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,13 @@
+import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
+import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
+import { AccountMongoRepository } from "../../infra/db/mongodb/accout-repository/account";
+import { SignUpController } from "../../presentation/controllers/signup/signup";
+import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+
+export const makeSignUpController = (): SignUpController => {
+  const emailValidatorAdapter = new EmailValidatorAdapter();
+  const bcryptAdapter = new BcryptAdapter();
+  const accountMongoRepository = new AccountMongoRepository();
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
+  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+};
