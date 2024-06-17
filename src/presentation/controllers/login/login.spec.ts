ea5e661f1a6b8cf95a456209b68af756ab3eadd6
Vini <viniczb@gmail.com>
2024-06-17 21:52:46 +0200

    refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,27 +1,37 @@
-import { MissingParamError } from "../../errors"
-import { badRequest } from "../../helpers/http-helpers"
-import { LoginController } from "./login"
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http-helpers";
+import { LoginController } from "./login";
 
-describe('Login Controller', () => {
-  test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController();
+  return {
+    sut,
+  };
+};
+
+describe("Login Controller", () => {
+  test("Should return 400 if no email is provided", async () => {
+    const {sut} = makeSut()
     const httpRequest = {
       body: {
-        password: 'any_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-  })
-  test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
+        password: "any_password",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
+  });
+  test("Should return 400 if no password is provided", async () => {
+       const {sut} = makeSut()
     const httpRequest = {
       body: {
-        email: 'any_email@mail.com'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
-  })
-
-})
+        email: "any_email@mail.com",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  });
+});
